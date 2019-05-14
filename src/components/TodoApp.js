217f850ea8f30cea62089c921f06600ddb177d7a
wikisiyun <wikisiyun@gmail.com>
2019-05-14 16:35:00 +0800

    - 新增、顯示項目

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,7 +1,58 @@
 import React, { Component } from 'react';
 
 class TodoApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      items: []
+    }
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  // 新增項目事件
+  handleFormSubmit(e) {
+    const { state: selfState } = this;
+    e.preventDefault();
+    // 判斷若存在則加入清單
+    if (selfState.text && selfState.text.trim()) {
+      this.setState((state) => ({
+        items: state.items.concat(state.text),
+        text: ''
+      }));
+    }
+  }
+  // 輸入欄位改變事件
+  handleInputChange(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
 
+  render() {
+    return (
+      <div>
+        <div>
+          <ul>
+            {
+              // 建立清單項目
+              this.state.items.map((item, key) => (
+                <li key={key}>
+                  {item}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div>
+          <form onSubmit={this.handleFormSubmit}>
+            <input type="text" value={this.state.text} onChange={this.handleInputChange} />
+            <button>新增</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TodoApp;
